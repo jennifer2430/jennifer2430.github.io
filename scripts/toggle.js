b055d82863fb51c3433bfe0bbf456908d2746c28
jennifer2430 <jchen243028@gmail.com>
2023-03-21 13:38:42 -0700

    Got rid of script console.log

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,7 +1,4 @@
 function toggle(link) {
-    console.log(link)
-    console.log(link.id)
     const courseID = `${link.id.match("(.*)-link")[1]}-content`
-    console.log(courseID)
     document.getElementById(courseID).classList.toggle("unseen")
 }
